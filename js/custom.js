8e38eafb7f4d6c5d0ce57deb4c2887bad375fab5
MaxSimakov <Simakov.maxim82@gmail.com>
2021-02-18 19:26:15 +0300

    created: calendar.html calendar-place.html

--- js/custom.js
+++ js/custom.js
@@ -5,4 +5,65 @@ $(document).ready(function () {
 		$(".theme-block").toggleClass('active');
 		$(this).toggleClass('active').next().slideToggle();
 	})
+  $('.past-years__items--js').slick({
+		dots: false,
+		infinite: false,
+		speed: 300,
+		slidesToShow: 3,
+		prevArrow: '<button type="button" class="slick-prev"></button>',
+		nextArrow: '<button type="button" class="slick-next"></button>',
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+					infinite: false,
+					dots: false
+				}
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1
+				}
+			},
+			// {
+			// 	breakpoint: 400,
+			// 	settings: {
+			// 		slidesToShow: 1,
+			// 		slidesToScroll: 1
+			// 	}
+			// },
+			
+			// You can unslick at a given breakpoint now by adding:
+			// settings: "unslick"
+			// instead of a settings object
+		]
+	});
+	
+	let objItem = document.querySelectorAll(".objects__item--js");
+	if(objItem) {
+		objItem.forEach(function (el) {
+			el.addEventListener('click', function () {
+				let objItem = document.querySelectorAll('.objects__item--js');
+				let self = this;
+
+				for (let item of objItem) {
+					let currContent = item.querySelector('.objects__content--js');
+
+					if (item === self) {
+						item.classList.toggle('active');
+						currContent.classList.toggle('active');
+					}
+					else {
+						item.classList.remove('active');
+						currContent.classList.remove('active');
+					}
+				}
+			})
+		});
+	}
 });
+
